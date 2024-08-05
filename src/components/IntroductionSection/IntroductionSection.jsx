55e24b74915bef93d14e4836c0b3d6077d94a18c
Lê Hữu Phúc <144695353+Alhphuc03@users.noreply.github.com>
2024-08-05 14:53:43 +0700

    Update IntroductionSection.jsx

--- src/components/IntroductionSection/IntroductionSection.jsx
+++ src/components/IntroductionSection/IntroductionSection.jsx
@@ -19,7 +19,7 @@ const IntroductionSection = () => {
           </p>
           <div className="cv-container">
             <a
-              href="https://drive.google.com/file/d/1BybuIU_R_oan_L0RCRa0Qmq-oOMrngUP/view"
+              href="https://drive.google.com/file/d/1Vgw-Svql-lDWaDjZppcWRupBwfVDcJmN/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="cv"
@@ -31,7 +31,7 @@ const IntroductionSection = () => {
       </div>
       <div className="about-img">
         <img
-          src="https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/425740001_122151152048034652_7480462945252868222_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=9eYkirJENz8Q7kNvgH-v12K&_nc_ht=scontent.fsgn5-8.fna&oh=00_AYBRzjmLLh69PIicoMyC0_-8BxBDwO1NfDaef-nxgf7_AA&oe=66A83326"
+          src="https://scontent.fsgn5-15.fna.fbcdn.net/v/t39.30808-6/425740001_122151152048034652_7480462945252868222_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHU8tEOokNTfPDcWSQzp_yykZlSCRAoIpyRmVIJECginEoj6AMHbmzqPsTV7yR7gHWrJ0By0ua6Y8qL6ImSqI4C&_nc_ohc=uY8OqpBLQ80Q7kNvgElyism&_nc_ht=scontent.fsgn5-15.fna&oh=00_AYD8vi_fMPJfx_unAouDi5TBQhftEDpgcZJkUlTCbhEOSg&oe=66B64326"
           alt="hero-image"
           className="img-avatar"
         />
